f6456cb8ad9b18055045ad4f046e2764c0c9e0bc
emogoektepe <e.goektepe@googlemail.com>
2023-10-19 21:15:50 +0200

    added add task first box

--- js/addTask.js
+++ js/addTask.js
@@ -5,7 +5,10 @@ function renderAddTask() {
         <div class="addTaskContent">
             <h1>Add Task</h1>
             <div class="form">
-                <div class="formLeftSide"></div>
+                <div class="formLeftSide">
+                    <span data-end="*">Title</span>
+                    <input type="text" name="" id="" placeholder="Enter a title">
+                </div>
                 <div class="formSeparator"></div>
                 <div class="formRightSide"></div>
             </div>
