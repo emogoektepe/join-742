4a0c087e0472d18676d9f7738c2c2b5e78e04f56
emogoektepe <e.goektepe@googlemail.com>
2023-10-21 14:27:34 +0200

    added global button classes

--- js/contacts.js
+++ js/contacts.js
@@ -3,7 +3,14 @@ function renderContacts() {
     let content = document.getElementById('content');
     content.innerHTML = /*html*/ `
         <div class="contactSection">
-            <button style="padding: 50px" class="buttonUnfilled">Test</button>
+            <div class="contactSectionHeader">
+                <div class="buttonFilled addNewContactButton">
+                    Add new contact
+                    <img src="../img/person_add.svg" alt="">
+                </div>
+            </div>
+            <div class="contacts">
+            </div>
         </div>
     `;
 
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 function init() {
-    renderSummary();
+    // renderSummary();
+    renderContacts();
 }
