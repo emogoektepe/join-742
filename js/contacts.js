f2a8b874727c39653c828875fcd748d96aae3e64
emogoektepe <e.goektepe@googlemail.com>
2023-10-21 15:02:14 +0200

    designed contacts

--- js/contacts.js
+++ js/contacts.js
@@ -2,14 +2,21 @@ function renderContacts() {
 
     let content = document.getElementById('content');
     content.innerHTML = /*html*/ `
-        <div class="contactSection">
-            <div class="contactSectionHeader">
-                <div class="buttonFilled addNewContactButton">
-                    Add new contact
-                    <img src="../img/person_add.svg" alt="">
+        <div class="contactsContent">
+            <div class="contactSection">
+                <div class="contactSectionHeader">
+                    <div class="buttonFilled addNewContactButton">
+                        Add new contact
+                        <img src="../img/person_add.svg" alt="">
+                    </div>
+                </div>
+                <div class="contacts">
                 </div>
             </div>
-            <div class="contacts">
+            <div class="infoSectionHeader">
+                <h1>Contacts</h1>
+                <div class="separator"></div>
+                <h3>Better with a team</h3>
             </div>
         </div>
     `;
