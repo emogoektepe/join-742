5f2a02c9945186c7ef8508e4bcfd1ae5410895cb
emogoektepe <e.goektepe@googlemail.com>
2023-10-22 16:01:42 +0200

    discord call

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 function init() {
-    renderSummary();
+    // renderSummary();
+    renderContacts();
 }
 
 function setActiveNav(activeId) {
     const navItems = document.querySelectorAll(".navItems div");
     navItems.forEach(item => {
         if (item.id === activeId) {
-            item.style.backgroundColor = "#4a5878";
+            item.style.backgroundColor = "#091931";
         } else {
             item.style.backgroundColor = ""; // Setzt die Hintergrundfarbe zurück
         }
