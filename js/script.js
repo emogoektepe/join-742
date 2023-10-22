68078ca3f30eb07f800bd685b24ce797b6359a6a
simonbrost <simonb@live.at>
2023-10-22 17:31:39 +0200

    hover and select

--- js/script.js
+++ js/script.js
@@ -7,8 +7,12 @@ function setActiveNav(activeId) {
     navItems.forEach(item => {
         if (item.id === activeId) {
             item.style.backgroundColor = "#091931";
+            item.lastElementChild.style.filter= "brightness(0) invert(1)";
+            item.style.color= "#ffffff";
         } else {
             item.style.backgroundColor = ""; // Setzt die Hintergrundfarbe zurück
+            item.lastElementChild.style.filter= "";
+            item.style.color= "";
         }
     });
 }
