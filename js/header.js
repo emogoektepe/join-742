902e722c61a186f828d8eb3fcfadafce58ba1241
simonbrost <simonb@live.at>
2023-10-28 20:38:34 +0200

    implemented header dropdownMenu

---
+++ js/header.js
@@ -0,0 +1,13 @@
+function renderHelp() {
+}
+function toggleDropdown() {
+    let dropdown = document.getElementById("dropdown");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
+
+// Funktion, um das Dropdown-Menü zu schließen, wenn außerhalb des Dropdown-DIVs geklickt wird.
+
