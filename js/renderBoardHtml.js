2326f2b1d7f1e3d786bc7f9ddbf63c07fdd45fc6
johannesbraun54 <johannesbraun02@gmail.com>
2023-11-09 15:50:33 +0100

    second mediaquery added

--- js/renderBoardHtml.js
+++ js/renderBoardHtml.js
@@ -16,13 +16,21 @@ function renderBoardHtml(){
                 Add Task
                 <img src="./img/add.svg">
             </div>
-            <div class="respBtn">
+            <div class="respBtn" onclick="openDialog('dialogAddTaskBoard','addTaskWindow')">
                 <img src="./img/responsiveAddButton.svg">
             </div>
             
         </div>
 
     </header>
+
+    <form class="respFormArea" action="#">
+        <input id="searchBoard" class="inputArea" onkeydown="updateBoardHtml()"  type="text" placeholder="Find Task">
+        <div>
+            <img src="./img/borderDash.svg">
+        </div> 
+            <img onclick="updateBoardHtml()" class="searchBoard" src="./img/search.svg">
+    </form>
 
     <div id="dialogAddTaskBoard" class="dialog-bgBoard d-none" onclick="closeDialog('dialogAddTaskBoard','addTaskWindow')">
                 <div id="addTaskWindow" style="transform: translateX(200%);" class="dialogAddTaskBoard" onclick="doNotClose(event)">
@@ -42,6 +50,9 @@ function renderBoardHtml(){
                     <div class="addTaskHeader filterBlue" onclick="openDialog('dialogAddTaskBoard','addTaskWindow')">
                         <img src="img/plusB.svg">
                     </div>
+                    <div class="respAddTaskHeader filterBlue" onclick="openDialog('dialogAddTaskBoard','addTaskWindow')">
+                        <img src="./img/responsiveAddPlus.svg">
+                    </div>
                 </div>
                 <div id="todo" ondrop="moveTo('todo')" class="todos" ondragover="allowDrop(event); highlight('todo')" ondragleave="removeHighlight('todo')">
                     <div class="noTasks">
@@ -55,6 +66,9 @@ function renderBoardHtml(){
                     <h3>In progress</h3>
                     <div class="addTaskHeader filterBlue" onclick="openDialog('dialogAddTaskBoard','addTaskWindow')">
                         <img src="img/plusB.svg">
+                    </div>
+                    <div class="respAddTaskHeader filterBlue" onclick="openDialog('dialogAddTaskBoard','addTaskWindow')">
+                        <img src="./img/responsiveAddPlus.svg">
                     </div>
                 </div>
                 <div id="inProgress" ondrop="moveTo('inProgress')" class="todos" ondragover="allowDrop(event); highlight('inProgress')" ondragleave="removeHighlight('inProgress')">
@@ -70,6 +84,9 @@ function renderBoardHtml(){
                     <div class="addTaskHeader filterBlue"  onclick="openDialog('dialogAddTaskBoard','addTaskWindow')">
                         <img src="img/plusB.svg">
                     </div>
+                    <div class="respAddTaskHeader filterBlue" onclick="openDialog('dialogAddTaskBoard','addTaskWindow')">
+                        <img src="./img/responsiveAddPlus.svg">
+                    </div>
                 </div>
                 <div id="awaitFeedback" ondrop="moveTo('awaitFeedback')" class="todos" ondragover="allowDrop(event); highlight('awaitFeedback')" ondragleave="removeHighlight('awaitFeedback')">
                     <div class="noTasks">
@@ -83,6 +100,9 @@ function renderBoardHtml(){
                     <h3>Done</h3>
                     <div class="addTaskHeader filterBlue" onclick="openDialog('dialogAddTaskBoard','addTaskWindow')">
                         <img src="img/plusB.svg">
+                    </div>
+                    <div class="respAddTaskHeader filterBlue" onclick="openDialog('dialogAddTaskBoard','addTaskWindow')">
+                        <img src="./img/responsiveAddPlus.svg">
                     </div>
                 </div>
                 <div id="done" class="todos" ondrop="moveTo('done')" ondragover="allowDrop(event); highlight('done')" ondragleave="removeHighlight('done')">
