5281632cb86932d5d0f2b30b687e4d2e1d6a7607
simonbrost <simonb@live.at>
2023-11-09 16:31:52 +0100

    implemented passwordMatching for register.js

--- js/register.js
+++ js/register.js
@@ -4,16 +4,18 @@ function initRegister() {
     loadUsers();
 }
 
-async function loadUsers(){
+async function loadUsers() {
     try {
-        let parseStorage = await getItem('users'); 
+        let parseStorage = await getItem('users');
         users = JSON.parse(parseStorage.data.value);
-    } catch(e){
+    } catch (e) {
         console.warning('Loading error:', e);
     }
 }
 
 async function register() {
+    passwordMatching();
+    // // Passwort-Validierung erfolgreich, Sie können den Registrierungsprozess fortsetzen
     users.push({
         email: userEmailInput.value,
         password: userPasswordInput.value,
@@ -23,6 +25,7 @@ async function register() {
     successfulRegistration();
 }
 
+
 function successfulRegistration() {
     const overlay = document.getElementById("overlay");
     overlay.style.display = "flex";
@@ -30,21 +33,39 @@ function successfulRegistration() {
     setTimeout(function () {
         overlay.style.display = "none";
         window.location.href = 'index.html'; //weiterleitung//
-    }, 700); 
+    }, 700);
+}
+function passwordMatching() {
+    let userPasswordInput = document.getElementById("userPasswordInput");
+    let confirmPasswordInput = document.getElementById("confirmPasswordInput");
+    let confirmPasswordContainer = document.getElementById("confirmPasswordContainer");
+    let confirmMsg = document.getElementById("confirmMsg");
+    let password = userPasswordInput.value;
+    let confirmPassword = confirmPasswordInput.value;
+
+    if (password !== confirmPassword) {
+        confirmPasswordContainer.style.borderColor = "red";
+        confirmMsg.style.display = "block"; // Fehlermeldung anzeigen
+        return false;
+    } else {
+        confirmPasswordContainer.style.borderColor = "";
+        confirmMsg.style.display = "none"; // Fehlermeldung ausblenden
+        return true;
+    }
 }
 function toggleAcceptCheckbox() {
     let checkedBox = document.getElementById('acceptBoxChecked');
     let uncheckedBox = document.getElementById('acceptBoxUnchecked');
 
-    if(checkedBox.style.display === "" || checkedBox.style.display === "none") {
-    checkedBox.style.display = "block";
-    uncheckedBox.style.display = "none";
-    }else{
-    checkedBox.style.display = "none";
-    uncheckedBox.style.display = "block";
+    if (checkedBox.style.display === "" || checkedBox.style.display === "none") {
+        checkedBox.style.display = "block";
+        uncheckedBox.style.display = "none";
+    } else {
+        checkedBox.style.display = "none";
+        uncheckedBox.style.display = "block";
     }
 }
- function resetForm() {
+function resetForm() {
     userEmailInput.value = '';
     userPasswordInput.value = '';
- }
+}
