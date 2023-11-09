b1d92201599d3ec2850abaff849c50cdd16f79b7
johannesbraun54 <johannesbraun02@gmail.com>
2023-11-09 14:27:19 +0100

    first mediaquery for board

--- js/renderBoardHtml.js
+++ js/renderBoardHtml.js
@@ -6,7 +6,7 @@ function renderBoardHtml(){
         </div>
         <div class="headBar">
             <form class="formArea" action="#">
-                <input id="searchBoard" class="inputArea"  type="text" placeholder="Find Task">
+                <input id="searchBoard" class="inputArea" onkeydown="updateBoardHtml()"  type="text" placeholder="Find Task">
                 <div>
                     <img src="./img/borderDash.svg">
                 </div> 
@@ -16,7 +16,12 @@ function renderBoardHtml(){
                 Add Task
                 <img src="./img/add.svg">
             </div>
+            <div class="respBtn">
+                <img src="./img/responsiveAddButton.svg">
+            </div>
+            
         </div>
+
     </header>
 
     <div id="dialogAddTaskBoard" class="dialog-bgBoard d-none" onclick="closeDialog('dialogAddTaskBoard','addTaskWindow')">
@@ -30,50 +35,62 @@ function renderBoardHtml(){
     </div>
 
 
-        <div class="statusHeader">
-            <div class="statusHeadline">
-                <h3 class="h3">To do</h3>
-                <div class="addTaskHeader filterBlue" onclick="openDialog('dialogAddTaskBoard','addTaskWindow')"><img src="img/plusB.svg"></div>
+        <div class="board">
+            <div class="todoSection">
+                <div class="statusheader">
+                    <h3>To do</h3>
+                    <div class="addTaskHeader filterBlue" onclick="openDialog('dialogAddTaskBoard','addTaskWindow')">
+                        <img src="img/plusB.svg">
+                    </div>
+                </div>
+                <div id="todo" ondrop="moveTo('todo')" class="todos" ondragover="allowDrop(event); highlight('todo')" ondragleave="removeHighlight('todo')">
+                    <div class="noTasks">
+                        No Tasks to do
+                    </div>
+                </div>
             </div>
-            <div class="statusHeadline">
-                <h3>In progress</h3>
-                <div class="addTaskHeader filterBlue" onclick="openDialog('dialogAddTaskBoard','addTaskWindow')"><img src="img/plusB.svg"></div>
-            </div>
-            <div class="statusHeadline">
-                <h3>Await feedback</h3>
-                <div class="addTaskHeader filterBlue"  onclick="openDialog('dialogAddTaskBoard','addTaskWindow')"><img src="img/plusB.svg"></div>
-            </div>
-            <div class="statusHeadline">
-                <h3>Done</h3>
-                <div class="addTaskHeader filterBlue" onclick="openDialog('dialogAddTaskBoard','addTaskWindow')"><img src="img/plusB.svg"></div>
-            </div>
-        </div>
 
-
-    <div class="board">
-
-        <div id="todo" ondrop="moveTo('todo')" class="todos" ondragover="allowDrop(event); highlight('todo')" ondragleave="removeHighlight('todo')">
-            <div class="noTasks">
-                No Tasks to do
+            <div class="todoSection">
+                <div class="statusheader">
+                    <h3>In progress</h3>
+                    <div class="addTaskHeader filterBlue" onclick="openDialog('dialogAddTaskBoard','addTaskWindow')">
+                        <img src="img/plusB.svg">
+                    </div>
+                </div>
+                <div id="inProgress" ondrop="moveTo('inProgress')" class="todos" ondragover="allowDrop(event); highlight('inProgress')" ondragleave="removeHighlight('inProgress')">
+                    <div class="noTasks">
+                        No Tasks to do
+                    </div>
+                </div>
             </div>
-        </div>
 
-        <div id="inProgress" ondrop="moveTo('inProgress')" class="todos" ondragover="allowDrop(event); highlight('inProgress')" ondragleave="removeHighlight('inProgress')">
-            <div class="noTasks">
-                No Tasks to do
+            <div class="todoSection">
+                <div class="statusheader">
+                    <h3>Await feedback</h3>
+                    <div class="addTaskHeader filterBlue"  onclick="openDialog('dialogAddTaskBoard','addTaskWindow')">
+                        <img src="img/plusB.svg">
+                    </div>
+                </div>
+                <div id="awaitFeedback" ondrop="moveTo('awaitFeedback')" class="todos" ondragover="allowDrop(event); highlight('awaitFeedback')" ondragleave="removeHighlight('awaitFeedback')">
+                    <div class="noTasks">
+                        No Tasks to do
+                    </div>
+                </div>
             </div>
-        </div>
 
-        <div id="awaitFeedback" ondrop="moveTo('awaitFeedback')" class="todos" ondragover="allowDrop(event); highlight('awaitFeedback')" ondragleave="removeHighlight('awaitFeedback')">
-            <div class="noTasks">
-                No Tasks to do
+            <div class="todoSection">
+                <div class="statusheader">
+                    <h3>Done</h3>
+                    <div class="addTaskHeader filterBlue" onclick="openDialog('dialogAddTaskBoard','addTaskWindow')">
+                        <img src="img/plusB.svg">
+                    </div>
+                </div>
+                <div id="done" class="todos" ondrop="moveTo('done')" ondragover="allowDrop(event); highlight('done')" ondragleave="removeHighlight('done')">
+                    <div class="noTasks">
+                        No Tasks to do
+                    </div>
+                </div> 
             </div>
-        </div>
-        <div id="done" class="todos" ondrop="moveTo('done')" ondragover="allowDrop(event); highlight('done')" ondragleave="removeHighlight('done')">
-            <div class="noTasks">
-                No Tasks to do
-            </div>
-        </div>
-    </div>`
+        </div>`
 
 }
