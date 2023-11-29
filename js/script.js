d444f10033138ca7ed452b5fda3156c328af31b7
simonbrost <simonb@live.at>
2023-11-29 08:12:38 +0100

    jsdoc for script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 let urlParams = new URLSearchParams(window.location.search);
 let parameterWert = urlParams.get('parameter');
 
+/**
+ * Initializes the application by including HTML, loading contacts from storage,
+ * initializing the registration process, loading tasks from storage, and rendering
+ * the appropriate content based on the URL parameters.
+ */
 async function init() {
     await includeHTML();
     loadContactsFromStorage();
@@ -18,6 +23,10 @@ async function init() {
     }
 }
 
+/**
+ * Hides various elements on the page, such as the header, summary, add task button, board, and contacts.
+ * Disables the click event on the navigation logo and sets its cursor style to default.
+ */
 function hideElements() {
     let headerRight = document.getElementById('headerRight');
     let summary = document.getElementById('summary');
@@ -34,6 +43,10 @@ function hideElements() {
     contacts.style.display = 'none';
 }
 
+/**
+ * Sets the active state for the navigation item with the given ID and removes the active state from other items.
+ * @param {string} activeId - The ID of the navigation item to set as active.
+ */
 function setActiveNavItem(activeId) {
     const navItems = document.querySelectorAll(".navItem");
     navItems.forEach(item => {
@@ -45,6 +58,9 @@ function setActiveNavItem(activeId) {
     });
 }
 
+/**
+ * Renders a greeting message based on the current time of day and updates HTML elements.
+ */
 function renderGreetingMessage() {
     const currentTime = new Date();
     const currentHour = currentTime.getHours();
@@ -65,6 +81,9 @@ function renderGreetingMessage() {
     getCurrentUserName();
 };
 
+/**
+ * Renders the user's initials based on the username from the URL parameters.
+ */
 function renderInitials() {
     let params = new URLSearchParams(window.location.search);
     let username = params.get('username');
@@ -80,16 +99,28 @@ function renderInitials() {
     }
 }
 
+/**
+ * Renders double initials based on the given names and the first characters of each name.
+ * @param {Array} names - An array containing the first and last names of the user.
+ * @param {string} firstNameCharacter - The first character of the user's first name.
+ */
 function renderDoubleInitials(names, firstNameCharacter) {
 
     let secondNameCharacter = names[1].charAt(0)
     document.getElementById('initials').innerHTML = `${firstNameCharacter}${secondNameCharacter}`;
 }
 
+/**
+ * Renders single initials based on the given first name character.
+ * @param {string} firstNameCharacter - The first character of the user's first name.
+ */
 function renderSingleinitials(firstNameCharacter) {
     document.getElementById('initials').innerHTML = `${firstNameCharacter}`;
 }
 
+/**
+ * Updates the HTML elements with the current user's name based on the username from the URL parameters.
+ */
 function getCurrentUserName() {
     let params = new URLSearchParams(window.location.search);
     let username = params.get('username');
@@ -97,7 +128,11 @@ function getCurrentUserName() {
     document.getElementById('mobileCurrentUserName').innerHTML = username
 }
 
-function loadJoinSummery(){
+/**
+ * Loads the join summary, redirects to 'index.html' for privacy policy and legal notice,
+ * or renders the summary based on the URL parameters.
+ */
+function loadJoinSummery() {
     if (parameterWert == 'privacyPolicy') {
         window.location.href = 'index.html';
     } else if (parameterWert == 'legalNotice') {
